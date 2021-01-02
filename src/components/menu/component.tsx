726230fe09d0ef46b50015d19604a93f6fb06f02
Aleksey Smirnov <smirnovale@ozon.ru>
2021-01-03 01:01:01 +0400

    Добавлена анимация активного элемента меню

--- src/components/menu/component.tsx
+++ src/components/menu/component.tsx
@@ -2,29 +2,73 @@ import React from 'react';
 import styles from './index.module.css';
 import { ItemProps, MenuProps } from './types';
 
-const MenuContext = React.createContext('');
+const MenuContext = React.createContext({
+  active: '',
+  updateActive: (element: HTMLDivElement) => {},
+});
+
+const useActive = (itemKey: string) => {
+  const { active, updateActive } = React.useContext(MenuContext);
+  const activeItemRef = React.useRef<HTMLDivElement>(null);
+  const isActive = active === itemKey;
+
+  React.useEffect(() => {
+    if (isActive && activeItemRef.current) {
+      updateActive(activeItemRef.current);
+    }
+  },
+  [isActive, updateActive]);
+
+  return {
+    isActive,
+    ref: activeItemRef,
+  };
+};
 
 const Item = ({ children, name, ...rest }: ItemProps) => {
-  const activeItem = React.useContext(MenuContext);
-  const isActive = activeItem === name;
-
-  const className = `${styles.item} ${isActive ? styles['item--active'] : ''}`;
-
+  const { ref } = useActive(name);
   return (
-    <div className={className} {...rest}>
+    <div ref={ref} className={styles.item} {...rest}>
       {children}
     </div>
   );
 };
 
-const Menu = ({ children, active }: MenuProps) => (
-  <MenuContext.Provider value={active}>
-    <div className={styles.menu}>
-      {children}
-    </div>
-  </MenuContext.Provider>
-);
+const Menu = ({ children, active }: MenuProps) => {
+  const [barStyles, setBarStyles] = React.useState({});
+  const menuRef = React.useRef<HTMLDivElement>(null);
+  const ctxValue = React.useMemo(() => ({
+    active,
+    updateActive: (el: HTMLDivElement) => {
+      setBarStyles(getRect(el, menuRef.current));
+    },
+  }), [active]);
+
+  return (
+    <MenuContext.Provider value={ctxValue}>
+      <div ref={menuRef} className={styles.menu}>
+        {children}
+        <div
+          className={styles['active-bar']}
+          style={barStyles}
+        />
+      </div>
+    </MenuContext.Provider>
+  );
+};
 
 Menu.Item = Item;
+
+function getRect(
+  element: HTMLDivElement,
+  parentElement: HTMLDivElement | null | undefined,
+) {
+  const rect = element.getBoundingClientRect();
+  const parentRect = parentElement ? parentElement.getBoundingClientRect() : { left: 0 };
+  return {
+    width: rect.width,
+    transform: `translateX(${rect.left - parentRect.left}px)`,
+  };
+}
 
 export { Menu };
